f5e117ea4e1af080fc6f3371f40c803eccb2cf56
Théo FIDRY <theo.fidry@gmail.com>
2021-05-14 10:36:57 +0200

    First version of the PoC

---
+++ src/Application/ConfigurableIO.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Fidry\Console package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\Console\Application;
+
+use Fidry\Console\IO;
+
+interface ConfigurableIO
+{
+    public function configureIO(IO $io): void;
+}
---
+++ src/Command/SymfonyCommand.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Fidry\Console package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\Console\Command;
+
+use Fidry\Console\IO;
+use Symfony\Component\Console\Command\Command as BaseSymfonyCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class SymfonyCommand extends BaseSymfonyCommand
+{
+    private Command $command;
+
+    /** @psalm-suppress PropertyNotSetInConstructor */
+    private IO $io;
+
+    public function __construct(Command $command)
+    {
+        $this->command = $command;
+
+        $name = $command->getConfiguration()->getName();
+
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $configuration = $this->command->getConfiguration();
+
+        $this
+            ->setDescription($configuration->getDescription())
+            ->setHelp($configuration->getHelp());
+
+        $definition = $this->getDefinition();
+
+        $definition->setArguments($configuration->getArguments());
+        $definition->setOptions($configuration->getOptions());
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->io = new IO($input, $output);
+
+        $command = $this->command;
+
+        if ($command instanceof InitializableCommand) {
+            $command->initialize($this->io);
+        }
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        $command = $this->command;
+
+        if ($command instanceof InteractiveCommand) {
+            $command->interact($this->io);
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return $this->command->execute($this->io);
+    }
+}
