4f3239e42baec95fe941a5254ab2cd4f4b0cb901
Théo FIDRY <theo.fidry@gmail.com>
2022-11-01 11:06:39 +0100

    Remove unused constant

--- src/Internal/Generator/GettersGenerator.php
+++ src/Internal/Generator/GettersGenerator.php
@@ -24,33 +24,6 @@ use function implode;
  */
 final class GettersGenerator
 {
-    private const TEMPLATE = <<<'PHP'
-    <?php
-
-    /*
-     * This file is part of the Fidry\Console package.
-     *
-     * (c) Théo FIDRY <theo.fidry@gmail.com>
-     *
-     * For the full copyright and license information, please view the LICENSE
-     * file that was distributed with this source code.
-     */
-
-    declare(strict_types=1);
-
-    namespace Fidry\Console;
-
-    use Fidry\Console\Internal\Type\TypeFactory;
-
-    /**
-     * @internal
-     */
-    trait IOGetters
-    {
-        // __GETTERS_PLACEHOLDER__
-    }
-    PHP;
-
     /**
      * @param non-empty-list<InputType> $types
      */
