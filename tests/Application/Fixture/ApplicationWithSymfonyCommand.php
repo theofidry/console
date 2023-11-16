33cf145749852986c6a7714cc7a731a355ddf8a2
Théo FIDRY <theo.fidry@gmail.com>
2023-11-16 09:10:28 +0100

    feat: Add support for using native Symfony commands
    Closes #195.

---
+++ tests/Application/Fixture/ApplicationWithSymfonyCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Fidry\Console package.
+ *
+ * (c) Théo FIDRY <theo.fidry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fidry\Console\Tests\Application\Fixture;
+
+use Fidry\Console\Application\BaseApplication;
+use Fidry\Console\Tests\Command\Fixture\SimpleSymfonyCommand;
+
+final class ApplicationWithSymfonyCommand extends BaseApplication
+{
+    public function getCommands(): array
+    {
+        return [
+            new SimpleSymfonyCommand(),
+        ];
+    }
+
+    public function getName(): string
+    {
+        return 'ApplicationWithSymfonyCommand';
+    }
+
+    public function getVersion(): string
+    {
+        return '0.0';
+    }
+
+    public function isAutoExitEnabled(): bool
+    {
+        return false;
+    }
+}
