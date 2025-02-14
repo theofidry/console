bea8316beae874fc5b8be679d67dd3169c7e205f
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2025-02-14 12:06:15 +0100

    feat: Allow psr/log 1.x and 2.x (#251)
    Closes #239

--- tests/IO/DummyLogger.php
+++ tests/IO/DummyLogger.php
@@ -14,29 +14,60 @@ declare(strict_types=1);
 namespace Fidry\Console\Tests\IO;
 
 use Closure;
+use Composer\InstalledVersions;
+use Composer\Semver\VersionParser;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LoggerTrait;
 use Stringable;
 use Symfony\Component\Console\Output\OutputInterface;
 use function func_get_args;
 
-final class DummyLogger implements LoggerInterface
-{
-    public array $records;
+$isPsrLog3 = InstalledVersions::satisfies(
+    new VersionParser(),
+    'psr/log',
+    '^3.0',
+);
 
-    use LoggerTrait;
-
-    /**
-     * @return Closure(OutputInterface):LoggerInterface
-     */
-    public static function getFactory(): Closure
+if ($isPsrLog3) {
+    final class DummyLogger implements LoggerInterface
     {
-        /** @psalm-suppress UnusedClosureParam */
-        return static fn (OutputInterface $output): LoggerInterface => new self();
+        public array $records;
+
+        use LoggerTrait;
+
+        /**
+         * @return Closure(OutputInterface):LoggerInterface
+         */
+        public static function getFactory(): Closure
+        {
+            /** @psalm-suppress UnusedClosureParam */
+            return static fn (OutputInterface $output): LoggerInterface => new self();
+        }
+
+        public function log(mixed $level, string|Stringable $message, array $context = []): void
+        {
+            $this->records[] = func_get_args();
+        }
     }
-
-    public function log(mixed $level, string|Stringable $message, array $context = []): void
+} else {
+    final class DummyLogger implements LoggerInterface
     {
-        $this->records[] = func_get_args();
+        public array $records;
+
+        use LoggerTrait;
+
+        /**
+         * @return Closure(OutputInterface):LoggerInterface
+         */
+        public static function getFactory(): Closure
+        {
+            /** @psalm-suppress UnusedClosureParam */
+            return static fn (OutputInterface $output): LoggerInterface => new self();
+        }
+
+        public function log($level, $message, $context = [])
+        {
+            $this->records[] = func_get_args();
+        }
     }
 }
